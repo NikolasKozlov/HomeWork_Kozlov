a000cd822258c9791b7c3230486083907a5cf66d
nikolas <ak2108zzz@gmail.com>
2021-07-21 13:34:59 +0300

    add

---
+++ 2.py
@@ -0,0 +1,3 @@
+my_dict = {'a': 400, 'b': 58700, 'c': -560,'d': 'one', 'e': 20000, 'f': 2000, 'd': 400}
+for key, value in my_dict.items():
+    print(value)
---
+++ 3.py
@@ -0,0 +1,2 @@
+text = input('enter words\n')
+print(text)
